04abab10b126fb43378846fdaf6610228fc2d714
Ryan Romanchuk <ryan@romanch.uk>
2023-02-19 23:44:18 -0600

    lazy deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 
-#!/usr/bin/env bash
+#!/bin/sh
+set -euxo pipefail;
+eval "$(./shdotenv)"
 
+yarn build
 scp -r ./skins ./bin ./install.py  pi@wx.local:/home/pi/weewx-wdc
+
+#aws s3 sync vendored/ s3://personal-www/static/weewx --storage-class INTELLIGENT_TIERING --cache-control max-age=31536000
 
 echo "sudo wee_extension --install weewx-wdc"
