30195d06f2ad86d080e04156702dbe33220434cb
Ryan Romanchuk <ryan@romanch.uk>
2024-04-27 14:20:47 -0500

    custom

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 
 #!/bin/sh
 set -euxo pipefail;
-eval "$(./shdotenv)"
+#eval "$(./shdotenv)"
 
-yarn build
+yarn run build
 scp -r ./skins ./bin ./install.py  pi@wx.local:/home/pi/weewx-wdc
 
 #aws s3 sync vendored/ s3://personal-www/static/weewx --storage-class INTELLIGENT_TIERING --cache-control max-age=31536000
 
-echo "sudo wee_extension --install weewx-wdc"
+echo "sudo weectl extension install -y weewx-wdc"
